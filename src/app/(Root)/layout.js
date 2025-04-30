6a1c0ddba2a0cdf03ead4cced055f7f4d94eb6fa
Obob <aubreynyasulu98@gmail.com>
2025-04-30 16:45:48 +0200

    fixed hero section appearing on places it is not meant to appear fix #1

--- src/app/(Root)/layout.js
+++ src/app/(Root)/layout.js
@@ -1,16 +1,11 @@
 import Footer from "@/UI/Components/Footer";
 import Nav from "@/UI/Components/Nav";
-import Hero from "@/UI/LandingPage/Sections/Hero";
 
 export default function RootLayout({ children }) {
   return (
     <>
       <Nav />
-
-      <Hero />
-
       {children}
-
       <Footer />
     </>
   );
