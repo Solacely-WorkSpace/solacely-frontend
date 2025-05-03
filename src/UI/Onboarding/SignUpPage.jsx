8f51752810f9cb7c393aad892ec37968c406455d
Obob <aubreynyasulu98@gmail.com>
2025-05-04 00:46:18 +0200

    removed duplicate components and added an OpenId button

--- src/UI/Onboarding/SignUpPage.jsx
+++ src/UI/Onboarding/SignUpPage.jsx
@@ -28,7 +28,11 @@ export default function SignUpPage() {
 
                     <p className="mt-10 text-xs text-center opacity-60">use your OpenId to Sign up</p>
 
-                    <button className="px-16 mt-4 rounded-full w-fit btn-primary">Google</button>
+                    <div className="mt-4 flex gap-4 items-center">
+                        <button className="px-16 rounded-full w-fit btn-primary">Google</button>
+
+                        <button className="px-16 rounded-full w-fit btn-primary bg-black">Facebook</button>
+                    </div>
 
                     <div className="w-[400px] h-[1px] bg-gray-400 opacity-20 mt-8 "></div>
 
