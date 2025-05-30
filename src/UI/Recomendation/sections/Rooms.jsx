fe8c2bd80f30131323e822a81f9e35bcf981b0db
Rez-create <okothbarack94@gmail.com>
2025-05-30 20:39:03 +0300

    fix of recommendation

--- src/UI/Recomendation/sections/Rooms.jsx
+++ src/UI/Recomendation/sections/Rooms.jsx
@@ -23,7 +23,7 @@ export default function Rooms({ setCurrentStage, currentStage }) {
         <section className='flex-1 w-full p-6 md:p-12'>
             <div className="w-full">
                 <div className="grid grid-cols-1 md:grid-cols-2 items-center w-full">
-                    <Link href="#" onClick={() => setCurrentStage('kind of home')} className="flex items-center gap-1 text-sm text-gray-600 hidden md:block">
+                    <Link href="#" onClick={() => setCurrentStage('home type')} className="flex items-center gap-1 text-sm text-gray-600 hidden md:block">
                         <div className="flex items-center gap-1">
                             <FiChevronLeft className="w-4 h-4 text-gray-600" /> Go back
                         </div>
