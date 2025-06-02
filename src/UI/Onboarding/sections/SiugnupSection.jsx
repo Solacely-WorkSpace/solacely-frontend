3b0944b11d9191977345f6ebceae8841526f6baa
Rez-create <okothbarack94@gmail.com>
2025-06-02 18:41:47 +0300

    Fix header text and remove Facebook button from signup section

--- src/UI/Onboarding/sections/SiugnupSection.jsx
+++ src/UI/Onboarding/sections/SiugnupSection.jsx
@@ -17,14 +17,13 @@ export default function SiugnupSection({ setCurrentStage }) {
             </div>
 
             <div className="flex flex-col items-center mt-16">
-                <h1 className="text-3xl">Login to Solacely</h1>
+                <h1 className="text-3xl">Sign up on Solacely</h1>
 
                 <p className="mt-10 text-xs text-center opacity-60">use your OpenId to Sign up</p>
 
                 <div className="mt-4 flex gap-4 items-center">
                     <button className="px-16 rounded-full w-fit btn-primary shadow-none">Google</button>
 
-                    <button className="px-16 rounded-full w-fit btn-primary shadow-none bg-black">Facebook</button>
                 </div>
 
                 <div className="w-[400px] h-[1px] bg-gray-400 opacity-20 mt-8 "></div>
