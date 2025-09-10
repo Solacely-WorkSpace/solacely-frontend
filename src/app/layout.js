d8904d09b1c6b90ada483063ac6aa60acb3d77a1
Rez-create <okothbarack94@gmail.com>
2025-09-10 19:38:48 +0300

    Add 360 icon animation and integrate Google Analytics and Clarity scripts

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import { Roboto } from "next/font/google"
+import Script from "next/script";
 import "@/Styles/globals.css";
 import Providers from "@/providers/QueryProvider";
 import { Toaster } from 'react-hot-toast';
@@ -21,6 +22,35 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${roboto.className} `}>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-6WQ6YX9LD2"
+          strategy="afterInteractive"
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-6WQ6YX9LD2');
+            `
+          }}
+        />
+        <Script
+          id="clarity-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "rcpdzwly5r");
+            `
+          }}
+        />
         <Providers>
           {children}
           <Toaster position="top-center" />
